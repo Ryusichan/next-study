9abb90a8e17dbc024dd462e5315f0ea181911167
Ryusichan <Ryusichan@github.com>
2022-07-19 17:06:23 +0900

    파일안의 html 내용 마크업하여 본문에 붙여넣기

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,6 +9,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
@@ -23,8 +25,9 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
   // Fetch necessary data for the blog post using params.id
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
